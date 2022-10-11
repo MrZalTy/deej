a9b9733d00b5b83d03adc187c1ae22c74ea704b2
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-10-11 02:51:36 +0200

    feat: added bot configuration

---
+++ src/config/bot.config.ts
@@ -0,0 +1,9 @@
+import * as env from 'env-var';
+
+export const botConfig = {
+	token: env.get('BOT_TOKEN').required(true).asString(),
+	logs: {
+		combinedPath: env.get('BOT_LOGS_COMBINED_PATH').default('logs/combined.log').asString(),
+		errorPath: env.get('BOT_LOGS_ERROR_PATH').default('logs/error.log').asString(),
+	},
+};
