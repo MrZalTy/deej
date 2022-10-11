4c5e2cb0dbe7d8fd868fb17afbacc03646493912
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-10-11 13:57:52 +0200

    feat: added commands manager

--- src/discord/discord.ts
+++ src/discord/discord.ts
@@ -1,22 +1,41 @@
-import * as DiscordJS from 'discord.js';
+import { Client, Collection } from 'discord.js';
 import * as fs from 'fs';
 import * as path from 'path';
 
 import { botConfig } from '../config';
-import { Event } from './discord.type';
+import { Command, Event } from './discord.type';
 
-const client = new DiscordJS.Client({
+const client = new Client({
 	intents: ['Guilds', 'GuildVoiceStates'],
 });
 
+function loadCommands(): void {
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('command.ts'));
+
+	commandFiles.map(async (commandFile) => {
+		const filePath = path.join(commandsPath, commandFile);
+		const command: Command = await import(filePath);
+
+		if (!command) {
+			return;
+		}
+		client.commands.set(command.data.name, command);
+		await client.application.commands.create(command.data.toJSON());
+	});
+}
+
 function loadEvents(): void {
 	const eventsPath = path.join(__dirname, 'events');
-	const eventsFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('event.ts'));
+	const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('event.ts'));
 
-	eventsFiles.map(async (eventFile) => {
+	eventFiles.map(async (eventFile) => {
 		const filePath = path.join(eventsPath, eventFile);
 		const event: Event = await import(filePath);
 
+		if (!event) {
+			return;
+		}
 		if (event.once) {
 			client.once(event.name, (...args) => event.execute(...args));
 		} else {
@@ -26,7 +45,10 @@ function loadEvents(): void {
 }
 
 async function init(): Promise<void> {
+	client.commands = new Collection<string, Command>();
+
 	await client.login(botConfig.token);
+	loadCommands();
 	loadEvents();
 }
 
