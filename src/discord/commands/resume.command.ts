8589b70ba2c7850ee33d04f0cf8238c5784d9d75
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-10-12 17:29:43 +0200

    feat: added resume command

---
+++ src/discord/commands/resume.command.ts
@@ -0,0 +1,38 @@
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+
+export = {
+	data: new SlashCommandBuilder().setName('resume').setDescription('Resume the player.'),
+	execute: async (interaction: CommandInteraction): Promise<void> => {
+		const { client, guild } = interaction;
+
+		if (!guild) {
+			return;
+		}
+
+		const queue = client.player.getQueue(guild);
+
+		if (!queue) {
+			await interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setTitle('**💿 │** No audio track')
+						.setDescription('There is no audio track in the queue.')
+						.setFooter({ text: `${client.user?.username}` }),
+				],
+				ephemeral: true,
+			});
+			return;
+		}
+
+		queue.setPaused(false);
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(`**▶ ️│** Player resumed`)
+					.setDescription('The player has been resumed.\n\nUse `/pause` to pause the player.')
+					.setFooter({ text: `${client.user.username}` }),
+			],
+			ephemeral: true,
+		});
+	},
+};
